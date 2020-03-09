f6799de8067058f8ac31fe5d538f9a666e77724c
Kajetan Schuler <kajetan.schuler@gmail.com>
2020-03-09 18:37:08 +0100

    New way of connecting to MySQL

--- app/models/db.js
+++ app/models/db.js
@@ -1,4 +1,33 @@
-const mysql = require('mysql');
+const mysql = require('mysql')
+const db_config = require('../config/db_config.js');
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: db_config.HOST,
+    user: db_config.USER,
+    password: db_config.PASSWORD,
+    database: db_config.DB
+})
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) connection.release()
+    console.log("MySQL connection pool initialized")
+    return
+})
+
+module.exports = pool
+
+/* const mysql = require('mysql');
 const db_config = require('../config/db_config.js');
 
 // Create a connection to the database
@@ -14,5 +43,4 @@ connection.connect(error => {
     if (error) throw error;
     console.log("Successfully connected to MySQL database")
 });
-
-module.exports = connection;
+ */
