bcee2fddfcba3ec0a02894c3faec49048af8a3ef
Kajetan Schuler <kajetan.schuler@gmail.com>
2020-04-07 13:52:31 +0200

    Added comments for missing routes

--- app/routes/country_routes.js
+++ app/routes/country_routes.js
@@ -3,4 +3,11 @@ module.exports = app => {
 
     // Find all countries and corresponding codes
     app.get("/api/v1/countries", country.findAll);
+
+    // Find all regions in country
+    
+    // Find all cities in country
+
+    
+
 }
