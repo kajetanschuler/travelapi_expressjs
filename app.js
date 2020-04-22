3ef894dc5fd4a444d7870c94d633bf4fa1940db8
Kajetan Schuler <kajetan.schuler@gmail.com>
2020-04-22 21:41:42 +0200

    CORS Enabled 5

--- app.js
+++ app.js
@@ -4,13 +4,36 @@ const serverless = require('serverless-http');
 const express = require("express");
 const bodyParser = require("body-parser");
 //const cors = require("cors");
-const allowCrossDomain = require("./app/middleware/cors.js");
+//const allowCrossDomain = require("./app/middleware/cors.js");
 
 const app = express();
 
+app.use(function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  });
+
+/*   module.exports.handler = function(event, context, callback) {
+
+    callback(null, {
+        statusCode: '200',
+        body: "Hello world",
+        headers: {
+            // This is ALSO required for CORS to work. When browsers issue cross origin requests, they make a 
+            // preflight request (HTTP Options) which is responded automatically based on SAM configuration. 
+            // But the actual HTTP request (GET/POST etc) also needs to contain the AllowOrigin header. 
+            // 
+            // NOTE: This value is *not* double quoted: ie. "'www.example.com'" is wrong
+            "Access-Control-Allow-Origin": "*"
+        }
+    });
+
+} */
 // Enable CORS for all Endpoints
 //app.use(cors());
-app.use(allowCrossDomain);
+//app.use(allowCrossDomain);
 
 // parse requests of content type: application/json
 app.use(bodyParser.json());
